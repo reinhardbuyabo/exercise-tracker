d94dff84d89fd876da9753cebc6a8a372f012732
reinhardbuyabo <buyabor@gmail.com>
2023-08-31 03:39:08 +0000

    Models

--- index.js
+++ index.js
@@ -3,6 +3,26 @@ const app = express()
 const cors = require('cors')
 require('dotenv').config()
 
+const mongoose = require('mongoose')
+const { Schema } = mongoose
+
+mongoose.connect(process.env.DB_URL)
+
+const UserSchema = new Schema({
+  username: String
+})
+
+const User = mongoose.model('User', UserSchema)
+
+const ExerciseSchema = new Schema ({
+  user_id: {type: String, required: true},
+  description: String, 
+  duration: Number, 
+  date: Date
+})
+
+const Exercise = mongoose.model('Exercise', ExerciseSchema)
+
 app.use(cors())
 app.use(express.static('public'))
 app.use(express.json())
@@ -44,6 +64,12 @@ app.post('/api/users/:_id/exercises', (req, res) => {
   })
 })
 })
+
+// GET /api/users/:_id/logs?[from][&to][&limit]
+app.get('/api/users/:id/logs?', (req, res)) {
+  { from, to, limit } = req.query // getting query parameters
+  
+}
 
 const listener = app.listen(process.env.PORT || 3000, () => {
   console.log('Your app is listening on port ' + listener.address().port)
